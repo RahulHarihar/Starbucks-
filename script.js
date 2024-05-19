164a007994e60d308561f55a13e24e2632f237b3
unknown <rahulharihar71@gmail.com>
2024-05-19 19:49:43 +0530

    Final Commit

--- script.js
+++ script.js
@@ -74,5 +74,14 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    const contactLink = document.getElementById('contact-link');
+    const contactSection = document.getElementById('contact');
+
+    contactLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        contactSection.scrollIntoView({ behavior: 'smooth'})
+    });
+});
 
 
